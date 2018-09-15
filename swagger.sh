c73b96f334ecf51e1b347d390920ca65542e0597
Thomas Denoréaz <thomas.denoreaz@gmail.com>
2018-09-15 04:16:12 +0200

    Android: Create App with Swagger

--- swagger.sh
+++ swagger.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-swagger-codegen generate -i swagger.yml -l android -o android
+swagger-codegen generate -i swagger.yml -l android -o android/swagger
 swagger-codegen generate -i swagger.yml -l nodejs-server -o nodejs
 swagger-codegen generate -i swagger.yml -l typescript-angular -o angular/src/swagger
