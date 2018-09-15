3fec04d682ccf8a537fa244fb14f79c45f59f574
Thomas Denoréaz <thomas.denoreaz@gmail.com>
2018-09-15 03:53:40 +0200

    NodeJS: Add TaskService implementation

--- nodejs/service/TaskService.js
+++ nodejs/service/TaskService.js
@@ -1,32 +1,64 @@
 'use strict';
 
+const mongodb = require('mongodb');
+
+const MongoClient = mongodb.MongoClient;
+
+const url = 'mongodb://localhost:32768';
+const dbName = 'test';
 
 /**
  * Add a new task
- * 
+ *
  *
  * body Task Task object that needs to be added
  * no response value expected for this operation
  **/
-exports.createTask = function(body) {
-  return new Promise(function(resolve, reject) {
-    resolve();
-  });
-}
+exports.createTask = function (body) {
+    return new Promise(function (resolve, reject) {
+        MongoClient.connect(url, (err, client) => {
+            const db = client.db(dbName);
+            const col = db.collection('tasks');
+
+            col.insertOne(body, (err, r) => {
+                if (err) {
+                    console.log('error', err);
+                    reject();
+                } else {
+                    console.log('resolved', r);
+                    resolve();
+                }
+            });
+        });
+    });
+};
 
 
 /**
  * Deletes a task
- * 
+ *
  *
  * taskId String Task id to delete
  * no response value expected for this operation
  **/
-exports.deleteTask = function(taskId) {
-  return new Promise(function(resolve, reject) {
-    resolve();
-  });
-}
+exports.deleteTask = function (taskId) {
+    return new Promise(function (resolve, reject) {
+        MongoClient.connect(url, (err, client) => {
+            const db = client.db(dbName);
+            const col = db.collection('tasks');
+
+            col.deleteOne({_id: projectId}, (err, r) => {
+                if (err) {
+                    console.log('error', err);
+                    reject();
+                } else {
+                    console.log('resolved', r);
+                    resolve();
+                }
+            });
+        });
+    });
+};
 
 
 /**
@@ -36,25 +68,24 @@ exports.deleteTask = function(taskId) {
  * taskId String ID of task to return
  * returns Task
  **/
-exports.getTaskById = function(taskId) {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = {
-  "end_date" : "2000-01-23T04:56:07.000+00:00",
-  "project_id" : "project_id",
-  "name" : "name",
-  "description" : "description",
-  "_id" : "_id",
-  "budget" : 0,
-  "start_date" : "2000-01-23T04:56:07.000+00:00"
+exports.getTaskById = function (taskId) {
+    return new Promise(function (resolve, reject) {
+        MongoClient.connect(url, (err, client) => {
+            const db = client.db(dbName);
+            const col = db.collection('tasks');
+
+            col.findOne({_id: taskId}, (project, err) => {
+                if (err) {
+                    console.log('error', err);
+                    reject();
+                } else {
+                    console.log('resolved', project);
+                    resolve(project);
+                }
+            });
+        });
+    });
 };
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
-  });
-}
 
 
 /**
@@ -62,46 +93,49 @@ exports.getTaskById = function(taskId) {
  *
  * returns List
  **/
-exports.readTasks = function() {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ {
-  "end_date" : "2000-01-23T04:56:07.000+00:00",
-  "project_id" : "project_id",
-  "name" : "name",
-  "description" : "description",
-  "_id" : "_id",
-  "budget" : 0,
-  "start_date" : "2000-01-23T04:56:07.000+00:00"
-}, {
-  "end_date" : "2000-01-23T04:56:07.000+00:00",
-  "project_id" : "project_id",
-  "name" : "name",
-  "description" : "description",
-  "_id" : "_id",
-  "budget" : 0,
-  "start_date" : "2000-01-23T04:56:07.000+00:00"
-} ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
-  });
-}
+exports.readTasks = function () {
+    return new Promise(function (resolve, reject) {
+        MongoClient.connect(url, (err, client) => {
+            const db = client.db(dbName);
+            const col = db.collection('tasks');
+
+            col.find({}).toArray((err, projects) => {
+                if (err) {
+                    console.log('error', err);
+                    reject();
+                } else {
+                    console.log('resolved', projects);
+                    resolve(projects);
+                }
+            });
+        });
+    });
+};
 
 
 /**
  * Updates a task
- * 
+ *
  *
  * taskId String ID of task that needs to be updated
  * body Task Task object that needs to be added
  * no response value expected for this operation
  **/
-exports.updateTask = function(taskId,body) {
-  return new Promise(function(resolve, reject) {
-    resolve();
-  });
-}
+exports.updateTask = function (taskId, body) {
+    return new Promise(function (resolve, reject) {
+        MongoClient.connect(url, (err, client) => {
+            const db = client.db(dbName);
+            const col = db.collection('tasks');
 
+            col.findOneAndUpdate({_id: taskId}, body, (projects, err) => {
+                if (err) {
+                    console.log('error', err);
+                    reject();
+                } else {
+                    console.log('resolved', projects);
+                    resolve();
+                }
+            });
+        });
+    });
+};
